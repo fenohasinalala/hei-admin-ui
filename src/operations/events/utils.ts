d219acbc36bcbc202d470159a5d522aa7131151e
JeanMarc RAJAONARIVELONA <127597326+JeanMarc-RAJAONARIVELONA@users.noreply.github.com>
2025-01-23 08:15:01 +0300

    feat: allow user to create frequency event and add color of event
    * chore: rename Event menu to Presence
    
    * build: client gen
    
    * fix: resolve event pagination issues
    
    * chore: change title field to imput select of classroom name
    
    * feat: add new field for assigning color codes to groups
    
    * build: clientgen
    
    * feat: enable users to create colors and select classrooms when creating events
    
    * feat: allow user to create frequency event
    
    * fix: resolve failing test

--- src/operations/events/utils.ts
+++ src/operations/events/utils.ts
@@ -4,7 +4,8 @@ export const EVENT_TYPE_VALUE = {
   SEMINAR: "Séminaire",
   SUPPORT_SESSION: "Entraide",
   OTHER: "Autre",
-};
+  EXAM: "Examen",
+} as const;
 
 export const ATTENDANCE_STATUS_VALUE = {
   MISSING: "Absent",
@@ -17,3 +18,23 @@ export const ATTENDANCE_STATUS_COLOR = {
   LATE: "info",
   PRESENT: "success",
 };
+
+export const CLASSROOM_CHOICES = [
+  {label: "Salle F", value: "F"},
+  {label: "Salle R", value: "R"},
+  {label: "Salle Pi", value: "Pi"},
+  {label: " Salle Sigma", value: "Sigma"},
+  {label: "Salle Algebre", value: "Algebre"},
+  {label: "Salle B", value: "B"},
+  {label: "Salle NP", value: "NP"},
+  {label: "Autre", value: "autre"},
+];
+
+export const RECURRENCE_TYPE_CHOICES = [
+  {value: "MONDAY", label: "Lundi"},
+  {value: "TUESDAY", label: "Mardi"},
+  {value: "WEDNESDAY", label: "Mercredi"},
+  {value: "THURSDAY", label: "Jeudi"},
+  {value: "FRIDAY", label: "Vendredi"},
+  {value: "SATURDAY", label: "Samedi"},
+];
